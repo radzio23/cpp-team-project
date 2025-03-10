f7cd66c82bb3dd0e9c9b26435830d9b8c417a129
zocha-jpg <zjedlinska@student.agh.edu.pl>
2025-03-10 16:02:46 +0100

    Update main.cpp
    dodana funkcja substract

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
   cout << "Witaj w naszym programie!" << endl;
   cout << "5 - 3 = " << subtract(5, 3) << endl;
