6cd825464c15fcb9e5163de7a234909ccc2d663d
radzio23 <150379327+radzio23@users.noreply.github.com>
2025-03-10 15:56:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-  cout << "Witaj w naszym programie!" << endl;
-  return 0;
-} 
+    cout << "Witaj w naszym programie!" << endl;
+    cout << "2 + 3 = " << add(2, 3) << endl;
+    return 0;
+}
