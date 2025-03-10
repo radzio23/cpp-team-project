d01482aebcab06af51e1956b5b27d3eba3f2fd70
zocha-jpg <zjedlinska@student.agh.edu.pl>
2025-03-10 16:03:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int subtract(int a, int b) {
-    return a - b;
-}
-
 int main() {
   cout << "Witaj w naszym programie!" << endl;
   cout << "5 - 3 = " << subtract(5, 3) << endl;
